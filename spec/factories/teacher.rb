6dd23bf26ef293c04f8e2b0f5bc295e785a616ad
Tony Headford <tony@binarycircus.com>
2024-07-05 22:56:04 +0100

    Reduce duplication in factory

--- spec/factories/teacher.rb
+++ spec/factories/teacher.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     transient do
       lead_provider { create(:lead_provider) }
       academic_year { create(:academic_year) }
+      with_declarations { false }
     end
 
     # lets see how messy this gets
@@ -13,17 +14,7 @@ FactoryBot.define do
         induction_period = create(:induction_period, ect_at_school_period:)
         provider_partnership = create(:provider_partnership, lead_provider: options.lead_provider, academic_year: options.academic_year)
         training_period = create(:training_period, induction_period:, provider_partnership:)
-      end
-    end
-
-    trait :in_ect_training_with_declarations do
-      after(:create) do |teacher, options|
-        ect_at_school_period = create(:ect_at_school_period, teacher:)
-        mentorship_period = create(:mentorship_period, mentee: ect_at_school_period)
-        induction_period = create(:induction_period, ect_at_school_period:)
-        provider_partnership = create(:provider_partnership, lead_provider: options.lead_provider, academic_year: options.academic_year)
-        training_period = create(:training_period, induction_period:, provider_partnership:)
-        create(:declaration, training_period:)
+        create(:declaration, training_period:) if options.with_declarations
       end
     end
   end
--- spec/services/declarations/find_spec.rb
+++ spec/services/declarations/find_spec.rb
@@ -3,12 +3,20 @@ require "rails_helper"
 RSpec.describe Declarations::Find do
   let(:lead_provider) { create(:lead_provider) }
   let(:academic_year) { create(:academic_year) }
-  let(:edmund) { create(:teacher, :in_ect_training_with_declarations, name: "Edmund", lead_provider:, academic_year:) }
-  let(:edmunds_declaration) { edmund.ect_at_school_periods.first.induction_periods.first.training_periods.first.declarations.first }
+  let(:edmund) { create(:teacher, :in_ect_training, with_declarations: true, name: "Edmund", lead_provider:, academic_year:) }
+  let!(:edmunds_declaration) { edmund.ect_at_school_periods.first.induction_periods.first.training_periods.first.declarations.first }
+  let(:ethel) { create(:teacher, :in_ect_training, with_declarations: true, name: "Ethel", academic_year:) }
+  let!(:ethels_declaration) { ethel.ect_at_school_periods.first.induction_periods.first.training_periods.first.declarations.first }
 
   subject(:service) { described_class.new(lead_provider:, academic_year:) }
 
-  it "should include edmunds declaration" do
-    expect(service.all.compact).to include [edmunds_declaration]
+  describe "#all" do
+    it "should include Edmund's declaration" do
+      expect(service.all.compact).to include [edmunds_declaration]
+    end
+
+    it "should not include Ethel's declaration" do
+      expect(service.all.compact).not_to include [ethels_declaration]
+    end
   end
 end
